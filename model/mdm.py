11ec571f8870ba0f256cb9e7c87a8c70c4c0c03a
Leo Bringer <leobr.1809@gmail.com>
2024-05-30 23:43:41 -0400

    cleaning

--- model/mdm.py
+++ model/mdm.py
@@ -50,9 +50,6 @@ class MDM(nn.Module):
         self.arch = arch
         self.use_gcn = use_gcn  # whether to use GCN for input and output processing
 
-        self.motion_input_linear = nn.Linear(self.input_feats, self.latent_dim)
-
-        # self.motion_input_gcn = GraphConvolution(self.input_feats, self.latent_dim, node_n=50, bias=True)
         if not self.use_gcn:
             self.input_process = InputProcess(self.data_rep, self.input_feats, self.latent_dim)
         else:
