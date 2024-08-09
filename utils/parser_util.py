8bc596663f13105d390d372c75993c0566605f57
Leo Bringer <leobr.1809@gmail.com>
2024-08-09 12:52:28 -0400

    parser update

--- utils/parser_util.py
+++ utils/parser_util.py
@@ -68,7 +68,7 @@ def add_diffusion_options(parser):
     group = parser.add_argument_group('diffusion')
     group.add_argument("--noise_schedule", default='cosine', choices=['linear', 'cosine'], type=str,
                        help="Noise schedule type")
-    group.add_argument("--diffusion_steps", default=50, type=int,
+    group.add_argument("--diffusion_steps", default=1000, type=int,
                        help="Number of diffusion steps (denoted T in the paper)")
     group.add_argument("--sigma_small", default=True, type=bool, help="Use smaller sigma values.")
 
@@ -78,8 +78,8 @@ def add_model_options(parser):
     group.add_argument("--arch", default='trans_enc',
                        choices=['trans_enc', 'trans_dec', 'gru'], type=str,
                        help="Architecture types as reported in the paper.")
-    group.add_argument("--use_gcn", default=True, type=bool)
-    group.add_argument("--emb_motion_len", default=50, type=int)
+    group.add_argument("--use_gcn", default=False, type=bool)
+    group.add_argument("--emb_motion_len", default=0, type=int)
     group.add_argument("--emb_trans_dec", default=False, type=bool,
                        help="For trans_dec architecture only, if true, will inject condition as a class token"
                             " (in addition to cross-attention).")
